f2865b01157d61ea0ae375ab04ba0dcb75fc3553
Joe Tingstad <70168043+joetingstad@users.noreply.github.com>
2021-09-03 09:35:54 -0700

    removed connection string for security

--- imports/db/testdb.js
+++ imports/db/testdb.js
@@ -1,7 +1,4 @@
 import { MongoClient } from "mongodb";
-
-const url = "mongodb+srv://CarlMaxeyAdmin:CarlMaxeyCenter@carlmaxeydb.m3vfj.mongodb.net/CarlMaxeyDB?retryWrites=true&w=majority";
-
   
 export const connectDatabase = () => {
     const client = MongoClient.connect(url);
